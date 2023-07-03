ab4c2cd47f81c8be60f0480dc1f0e297ca842891
Julian Casillas <jcasillasplaza@gmail.com@git config --global user.email jcasillasplaza@gmail.com>
2023-07-03 17:02:53 +0200

    cambios hechos

--- app.js
+++ app.js
@@ -1,34 +1,62 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require('dotenv/config');
+require("dotenv/config");
 
 // ℹ️ Connects to the database
-require('./db');
+require("./db");
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require('express');
+const express = require("express");
+const router = express.Router();
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require('hbs');
+const hbs = require("hbs");
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require('./config')(app);
+require("./config")(app);
 
 // default value for title local
-const projectName = 'lab-express-cinema';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const projectName = "lab-express-cinema";
+const capitalized = (string) =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
-const index = require('./routes/index');
-app.use('/', index);
+const Movie = require("./models/Movie.model");
+const index = require("./routes/index");
+app.use("/", index);
+
+router.get("/movies", async (req, res, next) => {
+  try {
+    const movies = await Movie.find();
+    res.render("movies", { movies });
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.get('/movies/:id', async (req, res, next) => {
+    try {
+      const movie = await Movie.findById(req.params.id);
+      if (!movie) {
+        return res.status(404).send('Movie not found');
+      }
+      res.render('movie', { movie });
+    } catch (error) {
+      console.error('Error fetching movie:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
+module.exports = router;
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require('./error-handling')(app);
+require("./error-handling")(app);
 
 module.exports = app;
