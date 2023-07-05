e3ecf1c413eb30c2fffb07d4e0cccdcd5e1a3686
Julian Casillas <jcasillasplaza@gmail.com@git config --global user.email jcasillasplaza@gmail.com>
2023-07-05 13:07:10 +0200

    mongodb direcciones editadas

--- seeds/movies.seed.js
+++ seeds/movies.seed.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Movie = require('../models/Movie.model');
 
 
-mongoose.connect('mongodb://localhost:27017/movies', { 
+mongoose.connect('mongodb://127.0.0.1:27017/movies', { 
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
